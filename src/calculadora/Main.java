eb4c3ff8032d02d652d5c2ab2afac82e962266c9
Cesar <cesar061093@hotmail.com>
2021-04-14 13:28:57 -0500

    Agrego funcion suma

--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Clase principal de calculadora");
     }
+
+    public void suma(){
+        System.out.println("Metodo para sumar");
+    }
+
 }
